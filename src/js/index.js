9dd39f3039d2742e71fab4ee59d637413860e38a
madhanm6277 <6277@velsrscollege.com>
2025-03-13 11:16:09 +0530

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,40 @@
 
+let isDarkTheme = false;
+
+window.addEventListener('contextmenu', (e)=> {
+    e.preventDefault()
+})
+
+function loadDarkTheme() {
+
+    const bodyElement = document.querySelector('body');
+    bodyElement.classList.toggle('dark-theme');
+    const icons = {
+        'sun': './assets/icons/sun.png',
+        'moon': './assets/icons/moon.png'
+    }
+    const imgElement = document.querySelector('.theme-icon');
+
+
+    if (isDarkTheme) {
+        imgElement.setAttribute('src', icons.moon)
+        
+        
+    }
+    else {
+        imgElement.setAttribute('src', icons.sun);
+    }
+    isDarkTheme = !isDarkTheme;
+
+
+}
+
+const downloadResume = (url, filename) => {
+    const link = document.createElement('a');
+    link.href = url;
+
+    link.donwload = filename || '';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
